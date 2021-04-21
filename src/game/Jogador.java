0da279b2bdc2764523eef0020273eb7c93b86116
cmcleon <77648947+cmcleon@users.noreply.github.com>
2021-04-21 19:44:19 -0300

    esboço da atv game - POO

---
+++ src/game/Jogador.java
@@ -0,0 +1,34 @@
+package game;
+
+public class Jogador {
+	//atributos
+	private String nome;
+	private int escolha;
+	
+	//construtores
+	public Jogador(String nome) {
+		this.nome = nome;
+	}
+
+	//geters and setters
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getEscolha() {
+		return escolha;
+	}
+
+	public void setEscolha(int escolha) {
+		this.escolha = escolha;
+	}
+
+	
+	
+	
+
+}
