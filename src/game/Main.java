c227fd6e3a8a2ed983c71ed62929bc5c24ed42a3
cmcleon <77648947+cmcleon@users.noreply.github.com>
2021-04-25 18:21:11 -0300

    :package: verificar nome e pção vazios

--- src/game/Main.java
+++ src/game/Main.java
@@ -5,6 +5,8 @@ package game;
 import java.util.Random;
 import java.util.Scanner;
 
+import senai.Usuario;
+
 
 public class Main {
 	public static void main(String[] args) {
@@ -12,16 +14,12 @@ public class Main {
 		Random geradorEscolha = new Random();
 		
 		System.out.println("--- Game JoKenPo --- ");
-		
-		System.out.print("Nome Jogador 1: ");
-		String nome= leitor.nextLine();
-		Jogador humano = new Jogador(nome);
+		//verificando se o usuário informou o nome
+		Jogador humano = new Jogador(verificarNome(leitor));
 		Jogador virtual = new Jogador();
 		
-		System.out.print("Escolha entre 1 = Pedra, 2 = Papel, 3 = Tesoura: ");
-		
-		int opcaoEscolhida = leitor.nextInt();
-		humano.setEscolha(opcaoEscolhida);
+		//verificando se o usuário escolheu opção entre 1 a 3
+		humano.setEscolha(verificarEscolha(leitor));
 		
 		System.out.println();
 		
@@ -30,10 +28,32 @@ public class Main {
 		virtual.setEscolha(opcaoEscolhidaPC);
 		
 		System.out.println();
+			
+		Jogar.obterVencedor(humano, virtual);		
+	}
+	
+	public static String verificarNome (Scanner leitor){
+		System.out.print("Nome Jogador 1: ");
+		String nome= leitor.nextLine();
 		
-		
-		Jogar.obterVencedor(humano, virtual);
-		
+		while (nome =="") {
+			System.out.print("Ops! Coloque seu nome: ");
+			nome= leitor.nextLine();
+			System.out.println();
+		}
+		return nome;
+	}
+
+	
+	public static int verificarEscolha (Scanner leitor){
+		System.out.print("Escolha entre 1 = Pedra, 2 = Papel, 3 = Tesoura: ");
+		int opcaoEscolhida = leitor.nextInt();
+
+		while (opcaoEscolhida == 0 || opcaoEscolhida > 3) {	
+			System.out.print("Ops! Você so pode escolher entre : 1 = Pedra, 2 = Papel, 3 = Tesoura: ");
+			opcaoEscolhida = leitor.nextInt();
+		}
+		return opcaoEscolhida;
 	}
 }
 		
